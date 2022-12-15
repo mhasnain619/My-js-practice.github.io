b46907abce294e173fc33053f1f9a28b401a75df
anain <hasnain0893@gmail.com>
2022-12-15 11:14:34 +0500

    my js pratice

---
+++ app.js
@@ -0,0 +1,9 @@
+//alert("hello world")
+let a = "hasnain"
+//console.log(a)
+const khaplu ="bala"
+//console.log(khaplu)
+var h ="balti"
+console.log(h,a,khaplu)
+const balti ="hundily"
+console.log(balti)
